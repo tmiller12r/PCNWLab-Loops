24f4e284180ba7ae80c17a50dd42e14a3058783e
Tony Miller <anthonymiller@pursuit.org>
2018-10-21 11:49:10 -0400

    Work on Loops pset2

--- pset2.js
+++ pset2.js
@@ -5,7 +5,17 @@
     @desc - takes all engative numbers and makes positive
     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
 */
-
+const removeNegatives = (arr) => {
+    const copyArr = [...arr];
+    for (let i = 0; i < arr.length; i++) {
+        if (copyArr[i] < 0) {
+        copyArr[i] *= -1
+        }
+    }
+return copyArr;
+}
+console.log(removeNegatives([-3,-2,-1,1,2,3,-4]));
+console.log(removeNegatives([-1000,-100,-10,0, -3, -4, -5]))
 /*
     @func findVal
     @param {array} arr
@@ -19,7 +29,21 @@
         findVal([1,2,3,4], 4); // 3
         findVal([1,2,3,4], 9); // -1
 */
+const findVal = (arr,val) => {
 
+    for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+        return i
+    }
+}
+return -1
+
+}
+console.log(findVal([1,2,3,4], 1));
+console.log(findVal([1,2,3,4], 4))
+console.log(findVal([1,2,3,4], 9))
+console.log(findVal([-11,9,43,-17], 9));
+console.log(findVal([6,7,8,9,-1], 6));
 /*
     @func removeOdds
     @param {array} arr
@@ -31,7 +55,17 @@
         removeOdds([1,2,3,4]); // [2,4]
         removeOdds([1,"2",3,4]); // [4]
 */
-
+const removeOdds = (arr)  => {    
+    const hasOddsArr = [];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0 && typeof(arr[i]) === "number") {
+        hasOddsArr.push(arr[i]);
+    }
+}
+return hasOddsArr;
+}
+console.log(removeOdds([1,2,3,4]));
+console.log(removeOdds([1,"2",3,4]));
 /*
     @func addSquares
     @param {array} arr
@@ -40,7 +74,16 @@
     @example - 
         addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
 */
+const addSquares = (arr) => {
+    const squares = [...arr];
+    for (let i = 0; i < arr.length; i++) {
+             
+         squares.push(arr[i] ** 2)
+    }
 
+    return squares;
+}
+console.log(addSquares([1,2,3,4]));
 /*
     @func doubleify
     @param {array} arr
@@ -48,8 +91,14 @@
     @desc - take each value of array, add same value right after
     @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
 */
-
-
+const doubleify = [] => {
+    const twinI = [...arr];
+    for let i = 0; i < arr.length; i++) {
+twinI.push(arr[i]) 
+}
+return twinI;
+}
+console.log(doubleify([1,2,3,4]));
 /*
     @func findMax
     @param {array}
